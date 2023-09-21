e1833b49efc6177493b87d704044fe14083f502d
DolphyWind <yunusaydin590@gmail.com>
2023-09-21 10:40:08 +0300

    Fix currency text and add icons for some currencies

--- startpage/MyStartPage/CurrencyManager.py
+++ startpage/MyStartPage/CurrencyManager.py
@@ -3,6 +3,20 @@ import requests
 class CurrencyManager:
     def __init__(self):
         self.api_url: str = "https://api.exchangerate.host/latest?base={}"
+        self.currency_icons = {
+            "TRY": "₺",
+            "USD": "$",
+            "EUR": "€",
+            "INR": "₹",
+            "GBP": "£",
+        }
+
+    def get_currency_icon(self, currency_name: str) -> str:
+        """Returns the icon of the currency. Returns the currency itself if it has no icon"""
+        if currency_name not in self.currency_icons.keys():
+            return currency_name
+
+        return self.currency_icons[currency_name]
 
     def fetch_currency(self, base_currency: str, target_currency: str) -> str:
         full_url = self.api_url.format(base_currency)
@@ -16,7 +30,16 @@ class CurrencyManager:
             return ""
 
         converted_currency = data['rates'][target_currency]
-        return f"1 {target_currency} = {round(converted_currency, 2)} {base_currency}"
+
+        base_currency = self.get_currency_icon(base_currency)
+        target_currency = self.get_currency_icon(target_currency)
+
+        if len(base_currency) != 1:
+            base_currency = " " + base_currency
+        if len(target_currency) != 1:
+            target_currency = " " + target_currency
+
+        return f"1{base_currency} = {round(converted_currency, 2)}{target_currency}"
 
     def fetch_multiple_currencies(self, currencyList: list[dict[str, str]]) -> list[str]:
         result: list[str] = []
